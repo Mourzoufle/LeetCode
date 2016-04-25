fc201f718c33df8e2d59490d6e476f111ec37732
Mourzoufle <Mourzoufle@163.com>
2016-04-25 20:23:08 +0800

    14. Longest Common Prefix

--- 014_Longest_Common_Prefix.cc
+++ 014_Longest_Common_Prefix.cc
@@ -4,16 +4,16 @@ public:
 		if (strs.empty())
 			return "";
 
-		int tail = -1;				// the last index of the common prefix
+		int tail = -1;			// the last index of the common prefix
 		while (++tail < strs[0].size()) {
-			bool is_equal = true;	// flag indicating if all the characters of the current index are the same
+			bool equal = true;	// flag indicating if all the characters of the current index are the same
 			for (int i = 0; i < strs.size(); i++) {
 				if ((tail == strs[i].size()) || (strs[i][tail] != strs[0][tail])) {
-					is_equal = false;
+					equal = false;
 					break;
 				}
 			}
-			if (!is_equal)
+			if (!equal)
 				break;
 		}
 		return tail == 0 ? "" : strs[0].substr(0, tail);
