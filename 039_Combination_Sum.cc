2bd19166daa69509eb0adc103a4395cdc23f9ce9
Mourzoufle <Mourzoufle@163.com>
2016-05-02 00:04:25 +0800

    39. Combination Sum

--- 039_Combination_Sum.cc
+++ 039_Combination_Sum.cc
@@ -1,25 +1,26 @@
 class Solution {
 public:
 	vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-		vector<vector<int>> solutions;
-		sort(candidates.begin(), candidates.end());
+		vector<vector<int>> combinations;																// valid combinations
+		sort(candidates.begin(), candidates.end());														// sort the numbers to determine the elements needed to be checked
 
-		vector<int> solution;
-		get_solutions(candidates, 0, target, solutions, solution);
-		return solutions;
+		vector<int> combination;
+		get_combinations(candidates, 0, target, combinations, combination);
+		return combinations;
 	}
 
 private:
-	void get_solutions(vector<int>& candidates, int head, int target, vector<vector<int>> &solutions, vector<int> solution) {
-		if (target == 0) {
-			solutions.push_back(solution);
+	/* get all possible combinations given the index of the first element to be checked */
+	void get_combinations(vector<int>& candidates, int head, int target, vector<vector<int>> &combinations, vector<int> &combination) {
+		if (target == 0) {																				// a valid combination found
+			combinations.push_back(combination);
 			return;
 		}
 
 		while ((head < candidates.size()) && (candidates[head] <= target)) {
-			solution.push_back(candidates[head]);
-			get_solutions(candidates, head, target - candidates[head], solutions, solution);
-			solution.pop_back();
+			combination.push_back(candidates[head]);
+			get_combinations(candidates, head, target - candidates[head], combinations, combination);	// continue to search with the current target
+			combination.pop_back();
 			head++;
 		}
 	}
