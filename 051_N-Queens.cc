494051db43aed768b00031c318c77b5cf0b43f54
Mourzoufle <Mourzoufle@163.com>
2016-05-11 23:54:49 +0800

    51. N-Queens

--- 051_N-Queens.cc
+++ 051_N-Queens.cc
@@ -1,42 +1,34 @@
 class Solution {
 public:
 	vector<vector<string>> solveNQueens(int n) {
-		vector<vector<string>> solutions;																// the resulting solutions
+		vector<vector<string>> solutions;			// the resulting solutions
+		vector<string> solution(n, string(n, '.'));	// the chess board
+		solveNQueens(0, solutions, solution);
 
-		int *cols = new int[n];																			// the index of the column that the queen in each row posits
-		for (int i = 0; i < n; i++)
-			cols[i] = -1;
-		int cur_idx = 0;																				// index of the row that the current queen posits
-		while (cur_idx >= 0) {																			// cur_idx < 0 indicates all possible positions of the queens are searched - no more solutions
-			while (++cols[cur_idx] < n) {																// loop until there is no possible column the current queen can posits
-				bool is_valid = true;																	// flag indicating if the position is valid
-				for (int i = 0; i < cur_idx; i++) {
-					if (cols[i] == cols[cur_idx] || (abs(cols[cur_idx] - cols[i]) == cur_idx - i)) {	// conflict occurs between two queens
-						is_valid = false;
-						break;
-					}
-				}
-				if (is_valid)
+		return solutions;
+	}
+
+private:
+	void solveNQueens(int row, vector<vector<string>> &solutions, vector<string> &solution) {
+		int size = solution.size();
+		if (row == size) {
+			solutions.push_back(solution);
+			return;
+		}
+
+		for (int i = 0; i < size; i++) {
+			bool is_valid = true;
+			for (int j = 0; j < row; j++) {
+				if ((solution[j][i] == 'Q') || ((i + row - j < size) && (solution[j][i + row - j] == 'Q')) || ((i + j - row >= 0) && (solution[j][i + j - row] == 'Q'))) {
+					is_valid = false;
 					break;
-			}
-			if (cols[cur_idx] == n)																		// no valid position of the current queen can be found - change the position of the former queen
-				cols[cur_idx--] = -1;
-			else {
-				if (cur_idx == n - 1) {																	// all queens are placed well - a solution found
-					vector<string> solution;
-					for (int i = 0; i < n; i++) {
-						string row;
-						for (int j = 0; j < n; j++)
-							row += (j == cols[i] ? 'Q' : '.');
-						solution.push_back(row);
-					}
-					solutions.push_back(solution);
-					cols[cur_idx--] = -1;
 				}
-				else																					// ...or more queens need to be placed
-					cur_idx++;
+			}
+			if (is_valid) {
+				solution[row][i] = 'Q';
+				solveNQueens(row + 1, solutions, solution);
+				solution[row][i] = '.';
 			}
 		}
-		return solutions;
 	}
 };
