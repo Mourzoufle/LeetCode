0b37ef63fcbfbdb1c1de9ea2156179cca372b090
Mourzoufle <Mourzoufle@163.com>
2016-05-12 00:17:42 +0800

    51. N-Queens

--- 051_N-Queens.cc
+++ 051_N-Queens.cc
@@ -1,23 +1,24 @@
 class Solution {
 public:
 	vector<vector<string>> solveNQueens(int n) {
-		vector<vector<string>> solutions;			// the resulting solutions
-		vector<string> solution(n, string(n, '.'));	// the chess board
+		vector<vector<string>> solutions;																																	// the resulting solutions
+		vector<string> solution(n, string(n, '.'));																															// current solution
 		solveNQueens(0, solutions, solution);
 
 		return solutions;
 	}
 
 private:
+	/* recursively find all valid positions of the queen in the current row */
 	void solveNQueens(int row, vector<vector<string>> &solutions, vector<string> &solution) {
 		int size = solution.size();
-		if (row == size) {
+		if (row == size) {																																					// all queens are checked and have valid positions - a solution found
 			solutions.push_back(solution);
 			return;
 		}
 
 		for (int i = 0; i < size; i++) {
-			bool is_valid = true;
+			bool is_valid = true;																																			// flag indicating if the current column is valid
 			for (int j = 0; j < row; j++) {
 				if ((solution[j][i] == 'Q') || ((i + row - j < size) && (solution[j][i + row - j] == 'Q')) || ((i + j - row >= 0) && (solution[j][i + j - row] == 'Q'))) {
 					is_valid = false;
