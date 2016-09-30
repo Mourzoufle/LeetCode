1ad05d37cb6b55531e89ffe7cc7322b22e1ad751
Mourzoufle <Mourzoufle@163.com>
2016-09-30 15:26:55 +0800

    final

--- 002_Add_Two_Numbers.cc
+++ 002_Add_Two_Numbers.cc
@@ -8,23 +8,28 @@
  */
 class Solution {
 public:
-	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-		ListNode *root = new ListNode((l1->val + l2->val) % 10);
-		int carry = (l1->val + l2->val >= 10 ? 1 : 0);
-		l1 = l1->next;
-		l2 = l2->next;
-		ListNode *cur_node = root;
-		while ((l1 != NULL) || (l2 != NULL) || (carry > 0)) {	// the next digit is nonzero
-			int sum = (l1 == NULL ? 0 : l1->val) + (l2 == NULL ? 0 : l2->val) + carry;
-			cur_node->next = new ListNode(sum >= 10 ? sum - 10 : sum);
-			carry = (sum >= 10 ? 1 : 0);
-			if (l1 != NULL)
-				l1 = l1->next;
-			if (l2 != NULL)
-				l2 = l2->next;
-			cur_node = cur_node->next;
-		}
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode(0);			// a dummy node pointing to the head
+        ListNode *cur_digit = dummy;				// current digit
+        bool carry = false;							// the carry digit - initially zero
 
-		return root;
-	}
+        while (l1 || l2 || carry) {					// loop until both l1 and l2 are empty, and there is no carry digit
+            cur_digit->next = new ListNode(0);
+            cur_digit = cur_digit->next;
+            if (l1) {
+                cur_digit->val += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                cur_digit->val += l2->val;
+                l2 = l2->next;
+            }
+            if (carry)
+                cur_digit->val++;
+            carry = cur_digit->val >= 10;
+            cur_digit->val %= 10;
+        }
+
+        return dummy->next ? dummy->next : dummy;	// check if there are actually no digit after the dummy node
+    }
 };
